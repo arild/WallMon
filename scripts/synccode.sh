93a6bd67169d2c6183d2b01769038de2d7de0df9
Arild Nilsen <arild.nilsen2@gmail.com>
2015-06-25 19:15:55 -0700

    Simplify synccode script

--- scripts/synccode.sh
+++ scripts/synccode.sh
@@ -1,26 +1,5 @@
 #!/bin/sh
 . $(dirname $0)/incl.sh
 
-# Synchronizes WallMon source code and related artifacts with provided destinations
-REMOTE_BUILD=false
-DEST=""
-for arg in "$@"
-do
-	if [ $arg == "c" ]; then
-		REMOTE_BUILD=true
-		continue
-	elif [ $arg == "ice" ]; then
-		DEST=arild@${ICE}
-	elif [ $arg == "rocksvv" ]; then
-		DEST=arild@${ROCKSVV}
-	else
-		echo "argument not defined: ${arg}"
-		continue
-	fi
-
-	echo "Syncing" ${WMON_DIR} "to" ${DEST}
-	${RSYNC} ${WMON_DIR} ${DEST}:.
-	if [ $REMOTE_BUILD == true ]; then
-		ssh ${DEST} "cd WallMon && make && exit"
-	fi
-done
+echo "Syncing" ${WMON_DIR} "to" ${DEST}
+${RSYNC} ${WMON_DIR} arild@${ROCKSVV}:.
